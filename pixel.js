992d1cee04d9a065af5aea1e3575baa674598188
M <mariia.khurdenko@gmail.com>
2019-05-13 20:14:08 +0300

    finish

--- pixel.js
+++ pixel.js
@@ -1,4 +1,3 @@
-
 let cellsArray = [];
 let colorEl = document.getElementsByClassName('color');
 let but = document.getSelection('button');
@@ -6,6 +5,7 @@ let curColsq = document.getElementById('square');
 let isDrawing = false;
 let cur_color = 'white';
 let inp_col = document.getElementById('input');
+let isFill = false;
 
 function start() {
     let width = prompt('Please enter desired width size', 10);
@@ -13,6 +13,7 @@ function start() {
     let place = document.getElementById('canvas');
     place.innerHTML = ('');
     isDrawing = false;
+    isFill = false;
     cur_color = 'white';
     curColsq.style.backgroundColor = cur_color;
     for (var i = 1; i <= height; i++) {
@@ -24,28 +25,33 @@ function start() {
     cellsArray = document.getElementsByClassName('cell');
     Array.prototype.forEach.call(cellsArray, function (elem) {
         elem.addEventListener('mousedown', event => {
-            event.target.style.backgroundColor = cur_color;
-            event.target.style.borderColor = cur_color;
-            isDrawing = true;
-        })
-    });
-    
-    Array.prototype.forEach.call(cellsArray, function (elem) {
-        elem.addEventListener('mousemove', e => {
-             if (isDrawing === true) {
+            if (isFill === true) {
+                filler(event.target, cur_color, event.target.style.backgroundColor);
+                isFill = false;
+            } else {
                 event.target.style.backgroundColor = cur_color;
                 event.target.style.borderColor = cur_color;
-             }
+                isDrawing = true;
+            }
         })
     });
-    
+
+    Array.prototype.forEach.call(cellsArray, function (elem) {
+        elem.addEventListener('mousemove', e => {
+            if (isDrawing === true) {
+                event.target.style.backgroundColor = cur_color;
+                event.target.style.borderColor = cur_color;
+            }
+        })
+    });
+
     Array.prototype.forEach.call(cellsArray, function (elem) {
         elem.addEventListener('mouseup', e => {
-             if (isDrawing === true) {
+            if (isDrawing === true) {
                 event.target.style.backgroundColor = cur_color;
                 event.target.style.borderColor = cur_color;
                 isDrawing = false;
-             }
+            }
         })
     });
 };
@@ -59,12 +65,52 @@ Array.prototype.forEach.call(colorEl, function (elem) {
     })
 });
 
-inp_col.addEventListener('change', function() {
+inp_col.addEventListener('change', function () {
     cur_color = inp_col.value;
     curColsq.style.backgroundColor = cur_color;
-}, false); 
+}, false);
 
 document.getElementById('button').addEventListener('click', start);
 
+document.getElementById('new').addEventListener('click', function () {
+    isFill = true;
+})
+    ;
 
+function filler(el, col, el_col) {
+    if (el == null) {
+        return;
+    }
+    el.style.backgroundColor = col;
+    el.style.borderColor = col;
+    if (el.nextSibling != null) {
+        if (el.nextSibling.style.backgroundColor == el_col)
+            filler(el.nextSibling, col, el_col);
+    }
 
+    if (el.previousSibling != null) {
+        if (el.previousSibling.style.backgroundColor == el_col)
+            filler(el.previousSibling, col, el_col);
+    }
+
+    let tt = 0;
+    let child = el;
+    while (child != null) {
+        tt++;
+        child = child.previousSibling;
+    }
+
+    if (el.parentElement.previousSibling != null) {
+        if (el.parentElement.previousSibling.childNodes[tt-1] != null) {
+        if (el.parentElement.previousSibling.childNodes[tt-1].style.backgroundColor == el_col)
+            filler(el.parentElement.previousSibling.childNodes[tt-1], col, el_col);
+    }
+}
+    if (el.parentElement.nextSibling != null) {
+        if (el.parentElement.nextSibling.childNodes[tt-1] != null) {
+        if (el.parentElement.nextSibling.childNodes[tt-1].style.backgroundColor == el_col)
+            filler(el.parentElement.nextSibling.childNodes[tt-1], col, el_col);
+    }
+}
+}
+;
